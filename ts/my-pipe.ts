9b649d2a0cca2c615a189190fd5c8a6cf5706b8a
Gary Taylor <gravity.addiction@gmail.com>
2016-05-10 11:00:06 -0700

    my-pipe class case fix

--- ts/my-pipe.ts
+++ ts/my-pipe.ts
@@ -3,7 +3,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({
   name: 'myPipe'
 })
-export class myPipe implements PipeTransform {
+export class MyPipe implements PipeTransform {
 
   // Example Pipe transform
   transform(value: any, args: any[] = null): string {
